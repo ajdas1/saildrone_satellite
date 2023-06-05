43a2501a2640988868a0bc6ff0c63f9a01e21833
ajdas1 <ajdas1@gmail.com>
2023-06-05 12:44:03 -0400

    do not plot every swath

--- scripts/data_exploration.py
+++ scripts/data_exploration.py
@@ -23,7 +23,7 @@ savedir = "/Users/asavarin/Desktop/work/saildrone_satellite/figs/test"
 datadir += f"/{product}"
 files = check_for_product(datadir=datadir, format = ".nc")
 
-for fl in files:
+# for fl in files:
 
-    data = read_swath(filename=f"{datadir}/{fl}", product=product)
-    plot_swath(data=data, variable="wind_speed", savefile=f"{savedir}/{fl}.png")
+#     data = read_swath(filename=f"{datadir}/{fl}", product=product)
+    # plot_swath(data=data, variable="wind_speed", savefile=f"{savedir}/{fl}.png")
