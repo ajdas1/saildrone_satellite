107d29d7e81f44c2e9575cab15935f602cf8a54a
ajdas1 <ajdas1@gmail.com>
2023-06-23 16:11:42 -0400

    update run

--- run.sh
+++ run.sh
@@ -4,4 +4,3 @@ python ./scripts/register_dataset.py
 python ./scripts/match_saildrone_satellite_swaths.py
 python ./scripts/plot_saildrone_satellite_data_match.py
 
-# python ./scripts/plot_saildrone_data_swath_coverage.py
