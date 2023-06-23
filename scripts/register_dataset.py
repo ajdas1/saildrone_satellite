57be68fd4f04e171602d2fca325b0111c5d7b1cd
ajdas1 <ajdas1@gmail.com>
2023-06-23 15:24:15 -0400

    update register_dataset

--- scripts/register_dataset.py
+++ scripts/register_dataset.py
@@ -1,6 +1,4 @@
-from file_check import read_config, register_new_dataset
-from read_product import DS
-
+from read_write import DS, read_config, register_new_dataset
 
 config = read_config()
 
@@ -8,9 +6,9 @@ if not config["register_dataset"]:
     exit()
 
 print(f"Registering a new dataset: {config['satellite_product']}")
-register_new_dataset(product=config["satellite_product"])
+register_new_dataset()
 
 supported_datasets = [e.value for e in DS]
 if config["satellite_product"] not in supported_datasets:
-    print("The shapefile processing for this dataset has not yet been implemented.")
-    print("You will need to add a processing step before continuing.")
+    print("     The processing for this dataset has not yet been implemented.")
+    print("     You will need to add a processing step before continuing.")
