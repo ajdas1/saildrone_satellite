d22479c83ff4c7bd3bd01768dcaf22437dbb16c5
ajdas1 <ajdas1@gmail.com>
2023-07-13 15:47:58 -0400

    put all scatter into main script

--- run.sh
+++ run.sh
@@ -1,3 +1,2 @@
 python ./scripts/match_saildrone_satellite_swaths.py
-python ./scripts/plot_all_saildrones_scatter.py
 
--- scripts/match_saildrone_satellite_swaths.py
+++ scripts/match_saildrone_satellite_swaths.py
@@ -1,3 +1,5 @@
+
+import os
 import pandas as pd
 import sys
 
@@ -8,15 +10,19 @@ from calculations import (
     get_saildrone_position_extrema,
 )
 from datetime import datetime
-from plot import plot_scatterplot_overlap, plot_timeseries_swath_overlap
+from plot import plot_matching_point_locations, plot_scatterplot_overlap, plot_timeseries_swath_overlap
 from read_write import (
     check_for_saildrone_data,
     check_for_satellite_data,
     create_data_folder_structure,
     DS,
+    fetch_repo_path,
+    get_sd_file_from_match_filename,
+    get_sat_file_from_match_filename,
     read_config,
     read_in_range_log,
     read_matching_data_from_file,
+    read_matching_data_from_file_product, 
     read_not_in_range_log,
     read_saildrone,
     read_swath,
@@ -24,6 +30,8 @@ from read_write import (
     write_matching_data_to_file,
     write_to_log,
 )
+
+
 
 
 config = read_config()
@@ -297,3 +305,101 @@ if len(combined) > 0:
 
 
 
+
+
+
+
+if not config["plot_all_saildrones_scatter"]:
+    sys.exit()
+
+print(
+    f"Plotting the scatterplot of all saildrones from "
+    + f"to {config['satellite_product']} satellite swaths."
+)
+
+
+sd_dirs = sorted([f"{fetch_repo_path()}/{config['matching_data_folder']}/{fl}" for fl in os.listdir(f"{fetch_repo_path()}/{config['matching_data_folder']}")])
+
+matching_fls = [[f"{sdir}/{config['satellite_product']}/{fl}" for fl in os.listdir(f"{sdir}/{config['satellite_product']}")] for sdir in sd_dirs]
+
+
+
+combined = []
+mean = []
+nearest = []
+matching_data = {}
+sd_files = []
+for sd_fls in matching_fls:
+    if len(sd_fls) == 0:
+        continue
+    sd_filename = get_sd_file_from_match_filename(filename=sd_fls[0], config=config)
+    sd_files.append(sd_filename)
+    sd_data = read_saildrone(filename=sd_filename, config=config, masked_nan=True, to_pd=True)
+    sd_data = sd_data.set_index(["time", "lat", "lon"])
+
+    tmp = []
+    for fl in sd_fls:
+        match_data = read_matching_data_from_file_product(filename=fl)
+        sat_filename = get_sat_file_from_match_filename(filename=fl, config=config)
+        sat_data = read_swath(filename=sat_filename, config=config, masked_nan=True, as_pd=True)
+        sat_data = sat_data.set_index(["time", "lat", "lon"])
+
+        comparison = match_saildrone_satellite_point(
+            match_data=match_data,
+            sd_data=sd_data,
+            st_data=sat_data,
+            sd_var=sd_var, 
+            st_var=st_var
+        )
+        comparison = comparison.dropna(thresh=5)
+
+        if len(comparison) == 0:
+            continue
+
+        tmp.append(match_data) 
+        combined.append(comparison)
+        mean.append(
+            comparison.groupby("sd_time", as_index=False).mean().reset_index(drop=True)
+        )
+        nearest.append(
+            comparison.loc[comparison.groupby("sd_time").dist.idxmin()].reset_index(drop=True)
+        )
+    matching_data[sd_filename] = tmp
+
+    
+combined = pd.concat(combined)
+nearest = pd.concat(nearest)
+mean = pd.concat(mean)
+combined = combined.sort_values(by=["sd_time", "st_time"]).reset_index(drop=True)
+nearest = nearest.sort_values(by=["sd_time", "st_time"]).reset_index(drop=True)
+mean = mean.sort_values(by=["sd_time", "st_time"]).reset_index(drop=True)
+
+
+
+
+filename = (
+    f"SD_"
+    + f"{config['satellite_product']}_scatter_overlap_"
+    + f"{config['saildrone_variable_name']}.png"
+)
+plot_scatterplot_overlap(
+    combined_pts=combined,
+    mean_pts=mean,
+    nearest_pts=nearest,
+    filename=filename,
+    axmin=0,
+    axmax=20,
+)
+
+filename = (
+    f"SD_"
+    + f"{config['satellite_product']}_matching_points_locations_"
+    + f"{config['saildrone_variable_name']}.png"
+)
+title = f"(SD - {config['satellite_product']}) matching point locations"
+plot_matching_point_locations(
+    match_data=matching_data,
+    sd_fls=sd_files,
+    filename=filename,
+    title=title,
+)
