6b686fd009210f3e9a24d310b5617ea49f22579d
ajdas1 <ajdas1@gmail.com>
2023-07-13 15:43:23 -0400

    moved timeseries plotting to main file

--- run.sh
+++ run.sh
@@ -1,5 +1,4 @@
 python ./scripts/match_saildrone_satellite_swaths.py
-python ./scripts/plot_saildrone_satellite_data_timeseries.py
 python ./scripts/plot_saildrone_satellite_data_scatter.py
 python ./scripts/plot_all_saildrones_scatter.py
 
