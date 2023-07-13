7c13782c48bfa46480a6bfb36d28c7b9c9840689
ajdas1 <ajdas1@gmail.com>
2023-07-13 16:02:05 -0400

    add original plotting functions back in

---
+++ scripts/plot_all_saildrone_scatter.py
@@ -0,0 +1,133 @@
+import os
+import pandas as pd
+
+from calculations import (
+    match_saildrone_satellite_point,
+)
+from plot import (
+    plot_matching_point_locations,
+    plot_scatterplot_overlap,
+)
+from read_write import (
+    fetch_repo_path,
+    get_sd_file_from_match_filename,
+    get_sat_file_from_match_filename,
+    read_config,
+    read_matching_data_from_file_product,
+    read_saildrone,
+    read_swath,
+)
+
+
+config = read_config()
+
+sd_var = config["saildrone_variable_name"]
+st_var = config["satellite_variable_name"]
+
+
+if config["plot_all_saildrones_scatter"]:
+    print(
+        f"Plotting the scatterplot of all saildrones from "
+        + f"to {config['satellite_product']} satellite swaths."
+    )
+
+    sd_dirs = sorted(
+        [
+            f"{fetch_repo_path()}/{config['matching_data_folder']}/{fl}"
+            for fl in os.listdir(
+                f"{fetch_repo_path()}/{config['matching_data_folder']}"
+            )
+        ]
+    )
+
+    matching_fls = [
+        [
+            f"{sdir}/{config['satellite_product']}/{fl}"
+            for fl in os.listdir(f"{sdir}/{config['satellite_product']}")
+        ]
+        for sdir in sd_dirs
+    ]
+
+    combined = []
+    mean = []
+    nearest = []
+    matching_data = {}
+    sd_files = []
+    for sd_fls in matching_fls:
+        if len(sd_fls) == 0:
+            continue
+        sd_filename = get_sd_file_from_match_filename(filename=sd_fls[0], config=config)
+        sd_files.append(sd_filename)
+        sd_data = read_saildrone(
+            filename=sd_filename, config=config, masked_nan=True, to_pd=True
+        )
+        sd_data = sd_data.set_index(["time", "lat", "lon"])
+
+        tmp = []
+        for fl in sd_fls:
+            match_data = read_matching_data_from_file_product(filename=fl)
+            sat_filename = get_sat_file_from_match_filename(filename=fl, config=config)
+            sat_data = read_swath(
+                filename=sat_filename, config=config, masked_nan=True, as_pd=True
+            )
+            sat_data = sat_data.set_index(["time", "lat", "lon"])
+
+            comparison = match_saildrone_satellite_point(
+                match_data=match_data,
+                sd_data=sd_data,
+                st_data=sat_data,
+                sd_var=sd_var,
+                st_var=st_var,
+            )
+            comparison = comparison.dropna(thresh=5)
+
+            if len(comparison) == 0:
+                continue
+
+            tmp.append(match_data)
+            combined.append(comparison)
+            mean.append(
+                comparison.groupby("sd_time", as_index=False)
+                .mean()
+                .reset_index(drop=True)
+            )
+            nearest.append(
+                comparison.loc[comparison.groupby("sd_time").dist.idxmin()].reset_index(
+                    drop=True
+                )
+            )
+        matching_data[sd_filename] = tmp
+
+    combined = pd.concat(combined)
+    nearest = pd.concat(nearest)
+    mean = pd.concat(mean)
+    combined = combined.sort_values(by=["sd_time", "st_time"]).reset_index(drop=True)
+    nearest = nearest.sort_values(by=["sd_time", "st_time"]).reset_index(drop=True)
+    mean = mean.sort_values(by=["sd_time", "st_time"]).reset_index(drop=True)
+
+    filename = (
+        f"SD_"
+        + f"{config['satellite_product']}_scatter_overlap_"
+        + f"{config['saildrone_variable_name']}.png"
+    )
+    plot_scatterplot_overlap(
+        combined_pts=combined,
+        mean_pts=mean,
+        nearest_pts=nearest,
+        filename=filename,
+        axmin=0,
+        axmax=20,
+    )
+
+    filename = (
+        f"SD_"
+        + f"{config['satellite_product']}_matching_points_locations_"
+        + f"{config['saildrone_variable_name']}.png"
+    )
+    title = f"(SD - {config['satellite_product']}) matching point locations"
+    plot_matching_point_locations(
+        match_data=matching_data,
+        sd_fls=sd_files,
+        filename=filename,
+        title=title,
+    )
---
+++ scripts/plot_saildrone_satellite_data_scatter.py
@@ -0,0 +1,91 @@
+import pandas as pd
+
+from calculations import (
+    match_saildrone_satellite_point,
+)
+from plot import (
+    plot_scatterplot_overlap,
+)
+from read_write import (
+    check_for_saildrone_data,
+    read_config,
+    read_in_range_log,
+    read_matching_data_from_file,
+    read_saildrone,
+    read_swath,
+)
+
+
+config = read_config()
+
+
+sd_var = config["saildrone_variable_name"]
+st_var = config["satellite_variable_name"]
+
+if config["plot_saildrone_satellite_data_scatter"]:
+    print(
+        f"Plotting the scatterplot of {config['saildrone_number']} from "
+        + f"{config['saildrone_year']} to "
+        + f"{config['satellite_product']} satellite swaths."
+    )
+
+    satellite_filenames = read_in_range_log(config=config)
+    if len(satellite_filenames) == 0:
+        print(
+            "     There are no satellite swaths that match the saildrone \nbased on the specified criteria."
+        )
+        exit()
+    saildrone_filename = check_for_saildrone_data(config=config)
+
+    saildrone_data = read_saildrone(
+        filename=saildrone_filename, config=config, masked_nan=True, to_pd=True
+    )
+    saildrone_data = saildrone_data.set_index(["time", "lat", "lon"])
+
+    match_data = read_matching_data_from_file(config=config, join_swaths=False)
+
+    combined = []
+    nearest = []
+    mean = []
+    for nfl, fl in enumerate(satellite_filenames):
+        swath_data = read_swath(filename=fl, config=config, masked_nan=True, as_pd=True)
+        swath_data = swath_data.set_index(["time", "lat", "lon"])
+        swath_match_data = match_data[nfl]
+
+        tmp = match_saildrone_satellite_point(
+            match_data=swath_match_data,
+            sd_data=saildrone_data,
+            st_data=swath_data,
+            sd_var=sd_var,
+            st_var=st_var,
+        )
+        tmp = tmp.dropna(axis=0, thresh=5)
+        if len(tmp) > 0:
+            combined.append(tmp)
+            mean.append(
+                tmp.groupby("sd_time", as_index=False).mean().reset_index(drop=True)
+            )
+            nearest.append(
+                tmp.loc[tmp.groupby("sd_time").dist.idxmin()].reset_index(drop=True)
+            )
+
+    if len(combined) > 0:
+        combined = pd.concat(combined).reset_index(drop=True)
+        nearest = pd.concat(nearest).reset_index(drop=True)
+        mean = pd.concat(mean).reset_index(drop=True)
+
+        filename = (
+            f"SD{config['saildrone_number']}."
+            + f"{config['saildrone_year']}_"
+            + f"{config['satellite_product']}_scatter_overlap_"
+            + f"{config['saildrone_variable_name']}.png"
+        )
+        plot_scatterplot_overlap(
+            combined_pts=combined,
+            mean_pts=mean,
+            nearest_pts=nearest,
+            filename=filename,
+            axmin=0,
+            axmax=20,
+        )
+
